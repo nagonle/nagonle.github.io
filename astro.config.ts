01348022db721fc67b026ba554104680c3642212
Nicolas Gonzalez <niandgonzalez@falabella.cl>
2024-12-28 10:25:58 -0300

    ci: fix deployment configs

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  base: '/nagonle.github.io/',
+  // base: 'nagonle.github.io',
   integrations: [
     tailwind({
       applyBaseStyles: false,
